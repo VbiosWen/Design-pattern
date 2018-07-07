71a38d6782e29139bb215030413b47197dad8842
vbisowen <wenliujie@qipeng.com>
2018-07-07 11:13:49 +0800

    开闭原则个人理解

--- design-pattern/src/main/java/com/vbiso/basic/refreshsmell/openingandclosingprinciple/Animal.java
+++ design-pattern/src/main/java/com/vbiso/basic/refreshsmell/openingandclosingprinciple/Animal.java
@@ -3,6 +3,10 @@ package com.vbiso.basic.refreshsmell.openingandclosingprinciple;
 /**
  * @Author: wenliujie
  * @Description:
+ * 开闭原则
+ * 在开闭原则下，通过一个抽象的Animal类，使得所有的动物继承此抽象类，然后再通过传入对象的方式
+ * 实现各个动物的操作。开闭原则在这种情况可以很好的帮助我们在进行动物的拓展，比如说再加一个dog，只需要
+ * 继承AbstractAnimal类就可以实现了，无需在修改其他类
  * @Date: Created in 下午6:34 2018/7/6
  * @Modified By:
  */
