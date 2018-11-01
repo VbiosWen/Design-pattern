9181b84d473fea4d2b6916c5f8295af6d3c44fe0
VbiosWen <1067477616@qq.com>
2018-11-01 19:35:11 +0800

    组合模式

---
+++ design-pattern/src/main/java/com/vbiso/basic/refreshsmell/decorated/Client.java
@@ -0,0 +1,11 @@
+package com.vbiso.basic.refreshsmell.decorated;
+
+/**
+ * @Author: wenliujie
+ * @Description:
+ * @Date: Created in 7:34 PM 2018/11/1
+ * @Modified By:
+ */
+public class Client {
+
+}
